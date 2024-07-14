b4903932321bdb566c2720cd2727ecbd59ab8fb2
Noman-Tanveer <nomantanveer021@gmail.com>
2024-07-14 17:46:17 +0500

    Made home page reactive

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
 import { faExternalLinkAlt } from '@fortawesome/free-solid-svg-icons';
+
 
 @Component({
   selector: 'app-header',
@@ -7,9 +9,29 @@ import { faExternalLinkAlt } from '@fortawesome/free-solid-svg-icons';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {
+  constructor(private router: Router) {}
+  menuActive: boolean = false;
   faExternalLinkAlt = faExternalLinkAlt
+  
+  ngOnInit(): void {
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        const tree = this.router.parseUrl(this.router.url);
+        if (tree.fragment) {
+          const element = document.getElementById(tree.fragment);
+          if (element) {
+            element.scrollIntoView({ behavior: 'smooth' });
+          }
+        }
+      }
+    });
+  }
   
   goToLink(url: string) {
     window.open(url, "_blank");
+  }
+
+  toggleMenu() {
+    this.menuActive = !this.menuActive;
   }
 }
