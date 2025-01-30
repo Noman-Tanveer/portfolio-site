0df446174498fc931e46fc5a17172be5604cc3a5
Your Name <you@example.com>
2025-01-31 02:15:32 +0500

    refactor: Simplify SegmentationService by removing unused imports and code

--- backend/src/segmentation.service.ts
+++ backend/src/segmentation.service.ts
@@ -1,19 +1,10 @@
 import { Injectable } from '@nestjs/common';                                                                                    
 
-import { Injectable } from '@nestjs/common';
-import { InjectModel } from '@nestjs/mongoose';
-import { Model } from 'mongoose';
-import { Image, ImageDocument } from './schemas/image.schema';
-
 @Injectable()
 export class SegmentationService {
-  constructor(@InjectModel(Image.name) private imageModel: Model<ImageDocument>) {}
+  constructor(@InjectModel(Image.name)) {}
 
   async saveImage(file: Express.Multer.File): Promise<Image> {
-    const createdImage = new this.imageModel({
-      data: file.buffer,
-      contentType: file.mimetype,
-    });
-    return createdImage.save();
+    file.saveImage(Image.name)
   }
 }
